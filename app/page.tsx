d9f970b111ce802bf3ab2927a355d42bcd6ac9c5
Quentin-Lecocq <quentin.lecocq@yahoo.com>
2025-01-05 20:35:03 +0100

    feat: create header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import ThemeToggle from '@/components/theme-toggle';
 import UploadForm from '@/features/upload/components/upload-form';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <ThemeToggle />
       <div className="text-center">
         <h1 className="text-4xl font-bold text-red-400">Kindian</h1>
         <h2 className="font-semibold text-gray-900 dark:text-gray-100">
---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+import ThemeToggle from './theme-toggle';
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-end p-4">
+      <ThemeToggle />
+    </header>
+  );
+};
+
+export default Header;
