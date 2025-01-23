c88d832a452a51b295cdee0d26f56fb2be259f0d
Quentin-Lecocq <quentin.lecocq@yahoo.com>
2025-01-23 08:31:44 +0100

    feat: add new pages and layout for home section, integrate Supabase SSR

---
+++ app/[locale]/(home)/statistics/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { NextPage } from 'next';
+import { useEffect, useState } from 'react';
+
+const StatisticsPage: NextPage = () => {
+  // const t = await getI18n();
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/hello')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return <>{message}</>;
+};
+
+export default StatisticsPage;
