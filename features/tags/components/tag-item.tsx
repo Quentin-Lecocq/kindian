2cc5b254c7bd8cdd3f9dc258a850c0c8459f7bec
Quentin-Lecocq <quentin.lecocq@yahoo.com>
2025-02-03 11:44:47 +0100

    refactor: update TagItem background color to use foreground theme

--- features/tags/components/tag-item.tsx
+++ features/tags/components/tag-item.tsx
@@ -10,7 +10,7 @@ type TagItemProps = {
 const TagItem = ({ tag, highlightId, onOptimisticDelete }: TagItemProps) => {
   const { id, name } = tag;
   return (
-    <div className="bg-neutral-300 flex items-center gap-1 px-1 py-1">
+    <div className="bg-foreground flex items-center gap-1 px-1 py-1">
       <p className="text-xs text-background pl-1">{name}</p>
       <DeleteTag
         highlightId={highlightId}
