7dce52cd4a7539a39af64ff8593f4c6e432bad9e
Quentin-Lecocq <112959342+Quentin-Lecocq@users.noreply.github.com>
2025-02-04 09:18:51 +0100

    Chore/setup for prod (#52)
    * chore: add .env.example and update .gitignore for environment configuration
    
    * refactor: migrate to dotenv-cli for environment configuration and update scripts
    
    * refactor: replace fetch API with Prisma for user creation in auth callback
    
    * refactor: prevent duplicate user creation in Supabase auth callback

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,3 +1,4 @@
+import { prisma } from '@/lib/prisma';
 import { createClient } from '@/supabase/server';
 import { getUser } from '@/utils/user';
 import { NextResponse } from 'next/server';
@@ -16,19 +17,22 @@ export async function GET(request: Request) {
 
     if (userData) {
       try {
-        await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/create`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            email: userData.email!,
-            supabaseId: userData.id,
-            name: userData.user_metadata?.full_name || null,
-            image: userData.user_metadata?.avatar_url || null,
-            createdAt: new Date().toISOString(),
-          }),
+        const existingUser = await prisma.user.findUnique({
+          where: { supabaseId: userData.id },
         });
+
+        if (!existingUser) {
+          await prisma.user.create({
+            data: {
+              email: userData.email!,
+              id: userData.id,
+              supabaseId: userData.id,
+              name: userData.user_metadata?.full_name || null,
+              image: userData.user_metadata?.avatar_url || null,
+              createdAt: new Date().toISOString(),
+            },
+          });
+        }
       } catch (error) {
         console.error("Erreur lors de l'appel à l'API:", error);
       }
