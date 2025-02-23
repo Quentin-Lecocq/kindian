f20d659d879c412e9bc17647c1a8a6a0d6a2168a
Quentin-Lecocq <quentin.lecocq@yahoo.com>
2025-02-23 13:11:13 +0100

    wip: last book section

--- features/home/component/last-book.tsx
+++ features/home/component/last-book.tsx
@@ -1,21 +1,26 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { prisma } from '@/lib/prisma';
 
 const LastBook = async () => {
   const lastBook = await getLastBook();
 
   return (
-    <div>
-      <h2 className="font-regular mb-8">Your last book</h2>
-      <div className="flex flex-col">
-        <h2 className="text-2xl font-bold">{lastBook?.title}</h2>
-        <p className="text-muted-foreground text-sm">
-          {lastBook?.author} - {lastBook?.pageCount} pages
-        </p>
-        <p className="text-sm mt-4 text-foreground">
-          {lastBook?.description?.slice(0, 400)}...
-        </p>
-      </div>
-    </div>
+    <Card className="p-6 rounded-sm">
+      <CardHeader>
+        <CardTitle>Your last book</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-col">
+          <h2 className="text-2xl font-bold">{lastBook?.title}</h2>
+          <p className="text-muted-foreground text-sm">
+            {lastBook?.author} - {lastBook?.pageCount} pages
+          </p>
+          <p className="text-sm mt-4 text-foreground">
+            {lastBook?.description?.slice(0, 400)}...
+          </p>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
